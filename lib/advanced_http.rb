1f90d24d8b4b1cc7f36cb669c8692a8476e57af2
Peter Williams <pwilliams@absolute-performance.com>
2007-12-04 14:18:03 -0700

    corrected some commects

--- lib/advanced_http.rb
+++ lib/advanced_http.rb
@@ -1,8 +1,6 @@
-
 require 'advanced_http/http_accessor'
 
-# AdvancedHttp is a facade that allows convenient access to the
-# functionality provided by the AdvancedHttp library.
+# AdvancedHttp is a library that provides a high level HTTP interface.
 module AdvancedHttp
   # A Hash of named URIs.  Many methods in AdvancedHTTP that need a URI
   # also take a name which they then resolve into a real URI using
