8d4ba569fc3f906d73ad75e1452ed14cecfbaafe
Paul Sadauskas <psadauskas@gmail.com>
2009-01-21 12:03:53 -0700

    Add headers defined in rcf2616 to the Header class.
        irb(main):001:0>  require 'lib/resourceful/header'
        => true
        irb(main):002:0>  Resourceful::Header::ACCEPT
        => "Accept"
        irb(main):003:0> h =  Resourceful::Header.new(Resourceful::Header::ACCEPT => 'text/html')
        => {"Accept"=>"text/html"}
        irb(main):004:0> h.accept
        => "text/html"

--- lib/resourceful/header.rb
+++ lib/resourceful/header.rb
@@ -25,6 +25,75 @@ module Resourceful
     def capitalize(k)
       k.to_s.downcase.gsub(/^.|[-_\s]./) { |x| x.upcase }.gsub('_', '-')
     end
+
+    HEADERS = %w[
+      Accept
+      Accept-Charset
+      Accept-Encoding
+      Accept-Language
+      Accept-Ranges
+      Age
+      Allow
+      Authorization
+      Cache-Control
+      Connection
+      Content-Encoding
+      Content-Language
+      Content-Length
+      Content-Location
+      Content-MD5
+      Content-Range
+      Content-Type
+      Date
+      ETag
+      Expect
+      Expires
+      From
+      Host
+      If-Match
+      If-Modified-Since
+      If-None-Match
+      If-Range
+      If-Unmodified-Since
+      Last-Modified
+      Location
+      Max-Forwards
+      Pragma
+      Proxy-Authenticate
+      Proxy-Authorization
+      Range
+      Referer
+      Retry-After
+      Server
+      TE
+      Trailer
+      Transfer-Encoding
+      Upgrade
+      User-Agent
+      Vary
+      Via
+      Warning
+      WWW-Authenticate
+    ]
+
+    HEADERS.each do |header|
+      const = header.upcase.gsub('-', '_')
+      meth  = header.downcase.gsub('-', '_')
+
+      class_eval <<-RUBY
+        #{const} = "#{header}".freeze    # ACCEPT = "accept".freeze
+
+        def #{meth}                      # def accept
+          self[#{const}]                 #   self[ACCEPT]
+        end                              # end
+
+        def #{meth}=(str)                # def accept=(str)
+          self[#{const}] = str           #   self[ACCEPT] = str
+        end                              # end
+      RUBY
+
+    end
+
   end
 end
 
