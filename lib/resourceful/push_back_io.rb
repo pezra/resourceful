14ed639ef595d81b7b605e5eec76b3580e1f0cc4
Peter Williams <pezra@barelyenough.org>
2009-02-26 09:04:51 -0700

    push back io completed

---
+++ lib/resourceful/push_back_io.rb
@@ -0,0 +1,68 @@
+# Copied from Rfuzz, w/ the bits I don't need removed.  Thanks Zed.
+require 'stringio'
+
+module Resourceful
+  # A simple class that using a StringIO object internally to allow for faster
+  # and simpler "push back" semantics.  It basically lets you read a random
+  # amount from a secondary IO object, parse what is needed, and then anything
+  # remaining can be quickly pushed back in one chunk for the next read.
+  class PushBackIo
+    attr_accessor :secondary
+
+    def initialize(secondary)
+      @secondary = secondary
+      @buffer = StringIO.new
+      @partial_supported = secondary.respond_to?(:readpartial)
+    end
+
+    # Pushes the given string content back onto the stream for the 
+    # next read to handle.
+    def push(content)
+      if content.length > 0
+        @buffer.write(content)
+      end
+    end
+    
+    # Read from internal buffer. If that is empty read from secondary IO.
+    def readpartial(maxlen)
+      r = pop(maxlen)
+      return r if r
+      # buffer was empty, read from secondary IO
+
+      if @partial_supported
+        return @secondary.readpartial(maxlen) 
+      else
+        raise EOFError if closed?
+        # We have an open IO from which to read 
+  
+        return @secondary.read(maxlen)
+      end
+    end
+
+    def write(content)
+      @secondary.write(content)
+    end
+
+    def flush
+      @secondary.flush
+    end
+
+    def close
+      @buffer.string = ""
+      @secondary.close
+    end
+
+    def closed?
+      @buffer.size == 0 && @secondary.closed? 
+    end
+
+    protected
+
+    def pop(n)
+      @buffer.rewind
+      o = @buffer.read(n)
+      @buffer.string = @buffer.read  # remove the stuff we have returned from the buffer.
+      return o
+    end
+  end
+end
