dccca477d5af31e1f692404253089f6af6c83b6e
Paul Sadauskas <psadauskas@gmail.com>
2009-07-15 14:30:29 -0600

    Version bump

--- lib/resourceful.rb
+++ lib/resourceful.rb
@@ -12,7 +12,7 @@ require 'resourceful/http_accessor'
 
 # Resourceful is a library that provides a high level HTTP interface.
 module Resourceful
-  VERSION = "0.5.2"
+  VERSION = "0.5.3"
   RESOURCEFUL_USER_AGENT_TOKEN = "Resourceful/#{VERSION}(Ruby/#{RUBY_VERSION})"
 
 end
